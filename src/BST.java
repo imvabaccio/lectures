d5153d3bdd4c5497efe6c8d33aeaf94e12ebf3b7
imvab <vaibhavbirlavb@gmail.com>
2022-10-06 20:52:57 +0530

    Updated BST

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,8 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class BST {
     TreeNode root;
 
@@ -14,7 +19,7 @@ public class BST {
             prev = temp;
             if (val < temp.value) {
                 temp = temp.left;
-            } else {
+            } else {// if (val > temp.value) {
                 temp = temp.right;
             }
         }
@@ -57,6 +62,25 @@ public class BST {
         }
     }
 
+    public void inOrder() {
+        if (root == null)
+            return;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+
+        TreeNode temp = root;
+
+        while (temp != null || !s.isEmpty()) {
+            while (temp != null) {
+                s.push(temp);
+                temp = temp.left;
+            }
+
+            temp = s.pop();
+            System.out.println(temp.value + " ");
+            temp = temp.right;
+        }
+    }
+
     public void postOrder(TreeNode root) {
         if (root != null) {
             postOrder(root.left);
@@ -65,11 +89,69 @@ public class BST {
         }
     }
 
+    public void postOrder() {
+        if (root == null)
+            return;
+
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        Stack<TreeNode> s2 = new Stack<TreeNode>();
+        s.push(root);
+
+        while (!s.isEmpty()) {
+            TreeNode curr = s.pop();
+            s2.push(curr);
+
+            if (curr.left != null)
+                s.push(curr.left);
+            if (curr.right != null)
+                s.push(curr.right);
+        }
+
+        while (!s2.isEmpty())
+            System.out.println(s2.pop().value);
+    }
+
     public void preOrder(TreeNode root) {
         if (root != null) {
             System.out.println(root.value);
             preOrder(root.left);
             preOrder(root.right);
+        }
+    }
+
+    public void preOrder() {
+        if (root == null)
+            return;
+
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(root);
+
+        while (!s.isEmpty()) {
+            TreeNode curr = s.pop();
+            System.out.println(curr.value + " ");
+
+            if (curr.right != null)
+                s.push(curr.right);
+            if (curr.left != null)
+                s.push(curr.left);
+        }
+    }
+
+    public void levelOrder() {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            double sum = 0;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode front = q.poll();
+                System.out.println(front.value);
+                if (front.left != null)
+                    q.offer(front.left);
+                if (front.right != null)
+                    q.offer(front.right);
+            }
         }
     }
 
